235d33816fb3e151499b4b88d4432ca936ceb914
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-06-17 16:26:20 +0300

    added admin dashboard

--- admin/index.php
+++ admin/index.php
@@ -9,9 +9,15 @@
 </head>
 <body>
     <!--Menu Section starts -->
-    <div class="menu">
+    <div class="menu text-center">
         <div class="wrapper">
-        menu goes here
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
         </div>
     </div>
     <!-- Menu section ends -->
@@ -19,7 +25,32 @@
      <!--Menu contents Section starts -->
      <div class="main-content">
      <div class="wrapper">
-        main content goes here
+        <h1><strong>DASHBOARD</strong></h1>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br />
+            Categories
+        </div>
+        
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br />
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br />
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br />
+            Categories
+        </div>
+
+        <div class="clearfix"></div>
         </div>
     </div>
     <!-- Menu contents section ends -->
