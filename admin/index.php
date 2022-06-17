0ddfac7b7bcb770596a7b1bdc022b274d32dfcc6
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-06-17 15:06:56 +0300

    admin panel

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>restosys | admin page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!--Menu Section starts -->
+    <div class="menu">
+        <div class="wrapper">
+        menu goes here
+        </div>
+    </div>
+    <!-- Menu section ends -->
+
+     <!--Menu contents Section starts -->
+     <div class="main-content">
+     <div class="wrapper">
+        main content goes here
+        </div>
+    </div>
+    <!-- Menu contents section ends -->
+
+     <!--footer Section starts -->
+     <div class="footer">
+     <div class="wrapper">
+         <p class="text-center">2022 allright reserved, restosys by MBAJE RWAKA ERIC - <a href="#">Proffesional programmer</a></p>
+        </div>
+    </div>
+    <!-- footer section ends -->
+</body>
+</html>
