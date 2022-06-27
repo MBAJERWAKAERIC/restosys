8262762ec78649915e1558016dcddf013067cf75
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-06-27 12:33:03 +0300

    login

---
+++ admin/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - food order system</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <div class="login">
+        <h1 class="text-center">Login</h1> <br><br>
+
+        <!--Login form starts here-->
+        <form action="" method="POST" class="text-center">
+            Username: <br>
+            <input type="text" name="username" placeholder="enter username"><br> <br>
+            password: <br>
+            <input type="password" name="password" placeholder="enter your password"><br><br>
+            <input type="submit" name="submit" value="Login" class="btn-primary"> <br><br>
+        </form>
+        <!--Login form ends here-->
+
+
+        <p class="text-center">Created By - <a href="http://mbajerwakaeric.000webhostapp.com/">MBAJE RWAKA ERIC</p>
+    </div>
+</body>
+
+</html>
