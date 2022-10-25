1a438d5163df88421e0059a1152fbbd8e9285e82
MBAJERWAKAERIC <erickmbanjerwaka@gmail.com>
2022-10-25 11:22:49 +0300

    login updated

--- admin/login.php
+++ admin/login.php
@@ -8,7 +8,7 @@
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Login - food order system</title>
+    <title>Login - food ordering system</title>
     <link rel='stylesheet' href='../css/admin.css'>
 </head>
 
